e21e39416268dd2d3889441a278aadc5afe5f228
CherryNoddles <69775709+CherryNoddles@users.noreply.github.com>
2020-09-14 09:17:24 +0800

    python_code.md

---
+++ python_code.md/hello.py/random.md/idk.md/python_code.py
@@ -0,0 +1,5 @@
+#This code should ask for your name, then scroll "Hello (your name) Have a fantastic day ahead!"
+#Please help me fix it. :)
+
+name = iput("What is your name?")
+print("Hello" + name  Have a fantastic day ahead!")
